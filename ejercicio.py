a86ec8b89c5058cc95b96c5acce760f4e1ec9928
Jorge201105 <jimunozacuna@gmail.com>
2025-06-12 02:13:14 +0000

    W

--- ejercicio.py
+++ ejercicio.py
@@ -13,3 +13,7 @@ paracetamol = Medicamento()
 print(paracetamol.descuento) # entrega el valor de descuento
 print(paracetamol.iva)# entrega el valor de iva
 
+precio =int(input("ingresa un valor"))
+
+print(paracetamol.validar_mayor_a_0(precio))
+
--- medicamento.py
+++ medicamento.py
@@ -1,6 +1,25 @@
 
 
 
+
 class Medicamento:
     descuento = 0.05
     iva = 0.19
+
+
+
+
+    @staticmethod
+    def validar_mayor_a_0(numero):
+        return numero > 0
+
+
+m1 = Medicamento()
+m2 = Medicamento()
+
+if m1.iva == m2.iva and m1.descuento == m2.descuento:
+    print("todas las instancias son iguales")
+    print(f"el iva es {Medicamento.iva}")
+    print(f"el descuento es {Medicamento.descuento}")
+
+
